5a5ba3505e1b2b115bb6875644ef0119e3137f93
Federico Sciarabba <sciarabbafederico@outlook.it>
2020-12-09 18:56:00 +0100

    Reso dinamici i title e le pagine

--- obj/Debug/netcoreapp2.2/Razor/Views/Home/Index.g.cshtml.cs
+++ obj/Debug/netcoreapp2.2/Razor/Views/Home/Index.g.cshtml.cs
@@ -7,14 +7,23 @@ namespace AspNetCore
 {
     #line hidden
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#line 2 "C:\Users\Federico\Desktop\MyCourse\Views\_ViewImports.cshtml"
+using System.Collections.Generic;
+
+#line default
+#line hidden
+#line 3 "C:\Users\Federico\Desktop\MyCourse\Views\_ViewImports.cshtml"
+using MyCourse.Models.ViewModels;
+
+#line default
+#line hidden
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d06e9f41e0b1db07491e94f758a08be915f62238", @"/Views/Home/Index.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"c66e443ba81bfd444e2b1c1ae94c4deedf2b8d44", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"54cd52ed39abecd474331a3ee15813f21cb4a7b5", @"/Views/_ViewImports.cshtml")]
     public class Views_Home_Index : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
